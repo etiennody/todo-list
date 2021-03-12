f0583922fede9ca1a681664442b7c83f939a6752
etiennody <etiennody@gmail.com>
2021-03-12 17:02:55 +0100

    fixed class meta+ used ModelSerializer

--- todo/serializers.py
+++ todo/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Todo
 
 
-class TodoSerializer(serializers.Serializer):
-    class class Meta:
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
         model = Todo
         fields = ("id", "title", "description", "completed")
