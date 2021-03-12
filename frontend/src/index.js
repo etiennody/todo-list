351317be74e8e409ab262fba26ff0d4c974dec0b
etiennody <etiennody@gmail.com>
2021-03-12 23:54:32 +0100

    fixed Legacy context api and findDOMNode with React.Fragment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,15 @@
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import App from './App';
+import App from './containers/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.Fragment>
     <App />
-  </React.StrictMode>,
+  </React.Fragment>,
   document.getElementById('root')
 );
 
